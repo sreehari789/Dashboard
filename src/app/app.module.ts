784952d69ab9584f51bd885fd4432845582ef848
sreehari789 <sreeharipraj3693@gmail.com>
2023-01-02 15:38:05 +0530

    Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { WidgetChartComponent } from './widget-chart/widget-chart.component';
+import { WidgetTableComponent } from './widget-table/widget-table.component';
+import { WidgetPieComponent } from './widget-pie/widget-pie.component';
+import { WidgetCardComponent } from './widget-card/widget-card.component';
+import{MatSidenavModule} from '@angular/material/sidenav'
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import { HighchartsChartModule } from 'highcharts-angular';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    SideNavComponent,
+    WidgetChartComponent,
+    WidgetTableComponent,
+    WidgetPieComponent,
+    WidgetCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
